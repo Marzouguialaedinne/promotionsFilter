f8f3f7de767237a221dcc9238057e078c241d919
Alaedinne Marzougui <alaedinne.marzougui@esprit.tn>
2023-03-26 14:52:13 +0200

    customize serializer Service

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,17 +3,17 @@
 namespace App\Controller;
 
 use App\DTO\EnquiryDto;
+use App\Service\Serializer\SerializerDto;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductController extends AbstractController
 {
 	#[Route(path: '/product/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
-	public function lowestPrice($id, Request $request, SerializerInterface $serializer)
+	public function lowestPrice($id, Request $request, SerializerDto $serializer)
 	{
 		if($request->headers->has('force_fail')) {
 			return new JsonResponse([
